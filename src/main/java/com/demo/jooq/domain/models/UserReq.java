e828c8085b1cdc23f73766dd975814e8d45d0275
keumhwan.moon <keumhwan.moon@lgepartner.com>
2025-02-27 15:52:43 +0900

    Add Javadoc comments to UserReq and UserRes DTO classes

--- src/main/java/com/demo/jooq/domain/models/UserReq.java
+++ src/main/java/com/demo/jooq/domain/models/UserReq.java
@@ -3,6 +3,12 @@ package com.demo.jooq.domain.models;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * User 요청 DTO
+ * <p>
+ * 사용자 생성, 수정 등의 요청에서 사용되는 데이터 전송 객체입니다.
+ * 이 클래스는 사용자 정보를 담고 있으며 요청 본문과 매핑됩니다.
+ */
 @Getter
 @Setter
 public class UserReq {
--- src/main/java/com/demo/jooq/domain/models/UserRes.java
+++ src/main/java/com/demo/jooq/domain/models/UserRes.java
@@ -5,6 +5,11 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+/**
+ * 사용자 응답 DTO
+ * <p>이 DTO는 사용자 정보를 반환하는 데 사용됩니다.
+ * 사용자 세부 정보를 포함합니다.
+ */
 @Getter
 @Setter
 public class UserRes {
